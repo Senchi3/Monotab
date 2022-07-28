548e204f6f2cd8fd5ecf326b7f7cf5b240668f1c
Senchi3 <sensensenchi@gmail.com>
2022-07-28 18:56:49 -0500

    No errors, midi being parsed correctly. No audio.

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-const MidiWriter = require('midi-writer-js');
-var MidiPlayer = require('midi-player-js');
+function playTextarea() {
+  // const MidiWriter = require("midi-writer-js");
+  // var MidiPlayer = require("midi-player-js");
 
+  const track = new MidiWriter.Track();
 
+  track.addEvent(
+    [
+      new MidiWriter.NoteEvent({ pitch: ["E4", "D4"], duration: "4" }),
+      new MidiWriter.NoteEvent({ pitch: ["C4"], duration: "2" }),
+      new MidiWriter.NoteEvent({ pitch: ["E4", "D4"], duration: "4" }),
+      new MidiWriter.NoteEvent({ pitch: ["C4"], duration: "2" }),
+      new MidiWriter.NoteEvent({
+        pitch: ["C4", "C4", "C4", "C4", "D4", "D4", "D4", "D4"],
+        duration: "8",
+      }),
+      new MidiWriter.NoteEvent({ pitch: ["E4", "D4"], duration: "4" }),
+      new MidiWriter.NoteEvent({ pitch: ["C4"], duration: "2" }),
+    ],
+    function (event, index) {
+      return { sequential: true };
+    }
+  );
 
-const track = new MidiWriter.Track();
+  const write = new MidiWriter.Writer(track);
+  console.log(write.dataUri("./test.mid"));
 
-track.addEvent([
-		new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-		new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-		new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-		new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-		new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
-		new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-		new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
-	], function(event, index) {
-    return {sequential: true};
-  }
-);
+  // Initialize player and register event handler
+  var Player = new MidiPlayer.Player(function (event) {
+    console.log(event);
+  });
 
-const write = new MidiWriter.Writer(track);
-console.log(write.dataUri('./test.mid'));
-
-
-
-// Initialize player and register event handler
-var Player = new MidiPlayer.Player(function(event) {
-	console.log(event);
-});
-
-// Load a MIDI file
-Player.loadFile(write.dataUri('./test.mid'));
-Player.play();
+  // Load a MIDI file
+  Player.loadDataUri(write.dataUri("./test.mid"));
+  Player.play();
+}
